c96f5f0ee02d6fea87f14ef2cfe2894035ed54ea
Arod8201 <80758612+Arod8201@users.noreply.github.com>
2021-04-06 10:50:24 +0800

    Update MultiArrayDemo4.java

--- src/main/java/com/ocp/day08/MultiArrayDemo4.java
+++ src/main/java/com/ocp/day08/MultiArrayDemo4.java
@@ -3,17 +3,13 @@ package com.ocp.day08;
 public class MultiArrayDemo4 {
 
     public static void main(String[] args) {
-        // 3人的身高與體重如下
         double[][] hw = {{170, 160, 180}, {60, 45, 90}};
+        double[] h = hw[0];
+        double[] w = hw[1];
 
-        // 求三人的 bmi
-        for (double[] x : hw) {
-            double h = x[0];
-            double w = x[1];
-            System.out.print(h);
-
-            //double bmi = w / Math.pow(h / 100, 2);
-            //System.out.printf("%.2f\n", bmi);
+        for (int i = 0; i < 3; i++) {
+            double bmi = w[i] / Math.pow(h[i] / 100, 2);
+            System.out.printf("%.2f\n", bmi);
         }
     }
 
