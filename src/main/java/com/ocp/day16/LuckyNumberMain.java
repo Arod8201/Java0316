28db1217b83ea2708823b332dd0d6f697acb460c
Arod8201 <80758612+Arod8201@users.noreply.github.com>
2021-04-22 23:00:46 +0800

    Update LuckyNumberMain.java

--- src/main/java/com/ocp/day16/LuckyNumberMain.java
+++ src/main/java/com/ocp/day16/LuckyNumberMain.java
@@ -5,16 +5,16 @@ import java.util.Random;
 public class LuckyNumberMain {
 
     public static void main(String[] args) {
-        // 利用 Lambda 實作一個可以得到 1~9 的功能
-        //LuckyNumber ln = r -> Math.random();
-        LuckyNumber ln =()-> 5;
-
-        // 利用 Lambda 實作一個可以得到 1~9 的功能
-        LuckyNumber ln1 =()-> {
-            
-            Random r=new Random();
-            int n =r.nextInt(49)+1;
-        return 5;      
+        // 利用 Lambda 實做一個可以得到 1~9 的功能
+        LuckyNumber num = () -> new Random().nextInt(9) + 1;
+        System.out.println(num.get());
+        
+        // 利用 Lambda 實做一個可以得到 1~49 的功能
+        LuckyNumber num2 = () -> {
+            Random r = new Random();
+            int n = r.nextInt(49) + 1;
+            return n;
         };
+        System.out.println(num2.get());
     }
 }
