8dc99243a67013df94f7171b9753f611e81a75af
Arod8201 <80758612+Arod8201@users.noreply.github.com>
2021-04-22 22:58:14 +0800

    Update TransactionMain.java

--- src/main/java/com/ocp/day16/TransactionMain.java
+++ src/main/java/com/ocp/day16/TransactionMain.java
@@ -5,10 +5,9 @@ import java.util.stream.Stream;
 public class TransactionMain {
 
     public static void main(String[] args) {
-        // amount=10, price=35
-        int num;
-        //Transaction t = ((amount, price) -> 
-           // System.out.println(amount + price)
+     // amount=10, price=35
+        Transaction t = (amount, price) -> System.out.println(amount * price);
+        t.print(10, 35);
         
     }
 }
