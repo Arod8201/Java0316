e24c0378cb51e6bd9c321a54bbe297c637fc55ef
Arod8201 <80758612+Arod8201@users.noreply.github.com>
2021-04-27 16:03:21 +0800

    Update NumberMain.java

--- src/main/java/com/ocp/day16/NumberMain.java
+++ src/main/java/com/ocp/day16/NumberMain.java
@@ -8,6 +8,20 @@ public class NumberMain {
         System.out.println(en.isEven(50));
 
         // Lambda 實作 PrimeNumber, n = 9973 是否是質數
-        
+                PrimeNumber pn = n -> {
+            if (n == 1) {
+                return false;
+            }
+            boolean ok = true;
+            int nn = (int) (Math.sqrt(n));
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    ok = false;
+                    break;
+                }
+            }
+            return ok;
+        };
+        System.out.println("是否為質數: " + pn.isPrime(9973));
     }
 }
