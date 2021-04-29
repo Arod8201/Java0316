195387446cdb111c6dac24c1c3682a62856ca9ac
MB-study <MB-study@192.168.1.152>
2021-04-29 21:43:25 +0800

    Java

---
+++ src/main/java/com/ocp/day17/LambdaDemo2.java
@@ -0,0 +1,18 @@
+package com.ocp.day17;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.stream.Stream;
+
+public class LambdaDemo2 {
+
+    public static void main(String[] args) {
+        // 求此人的總成績和平均
+        DoubleSummaryStatistics stat = Stream.of("國文", 100.0, "英文", 90.5, "數學", 70.5, "座號", 7)
+                .filter(x -> x instanceof Double)
+                .peek(x -> System.out.println(x.getClass().getTypeName()))
+                .mapToDouble(x -> (double) x) // 物件 Double 轉基本 double 才會變成 DoubleStream
+                .summaryStatistics();
+        System.out.println(stat.getSum());
+        System.out.println(stat.getAverage());
+    }
+}
---
+++ src/main/java/com/ocp/day18/School.java
@@ -0,0 +1,50 @@
+package com.ocp.day18;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.function.Consumer;
+
+public class School {
+
+    public static void main(String[] args) {
+        Arrays.stream(DataCenter.getPeople())
+                .forEach(System.out::println);
+        System.out.println("=========================");
+
+        // 請問考試的總分與平均 = ?
+        IntSummaryStatistics stat = Arrays.stream(DataCenter.getPeople())
+                .filter(s -> s instanceof Student)
+                //.peek(s -> System.out.println(s.getClass().getTypeName()));
+                .map(s -> (Student) s)
+                .mapToInt(s -> s.getScore())
+                .summaryStatistics();
+        System.out.println("學生的總分: " + stat.getSum());
+        System.out.println("學生的總分平均: " + stat.getAverage());
+        System.out.println("=========================");
+
+        // 請問老師的薪資與平均 = ?
+        IntSummaryStatistics stat1 = Arrays.stream(DataCenter.getPeople())
+                .filter(s -> s instanceof Teacher)
+                .map(s -> (Teacher) s)
+                .mapToInt(s -> s.getSalary())
+                .summaryStatistics();
+        System.out.println("老師的薪資: " + stat1.getSum());
+        System.out.println("老師的薪資平均: " + (int) stat1.getAverage());
+        System.out.println("=========================");
+
+        // 印出不及格的學生姓名與分數 , 老師姓名 , 不及格的總平均
+        Consumer<Student> p = s -> System.out.printf("學生:%s, 分數:%d, 老師:%s\n", s.getName(), s.getScore(), s.getTeacher().getName());
+        double avg = Arrays.stream(DataCenter.getPeople())
+                .filter(s -> s instanceof Student)
+                //.peek(s -> System.out.println(s.getClass().getTypeName()));
+                .map(s -> (Student) s)
+                .filter(s -> s.getScore() < 60)
+                .peek(p)
+                .mapToInt(Student::getScore)
+                .average()
+                .getAsDouble();
+        System.out.println("=========================");
+        System.out.println("不及格平均: " + avg);
+        System.out.println("=========================");
+    }
+}
