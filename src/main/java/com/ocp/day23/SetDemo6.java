ea269e266ff0dc4c9c782f968fc798ad7a6d26ce
Arod8201 <80758612+Arod8201@users.noreply.github.com>
2021-05-07 21:30:56 +0800

    Update SetDemo6.java

--- src/main/java/com/ocp/day23/SetDemo6.java
+++ src/main/java/com/ocp/day23/SetDemo6.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 public class SetDemo6 {
 
     public static void main(String[] args) {
-         // 利用 HastSet 加入資料
+           // 利用 HastSet 加入資料
         Set<Integer> data = new HashSet<>();
         data.add(100);
         data.add(80);
@@ -18,9 +18,8 @@ public class SetDemo6 {
 
         // 利用 TreeSet 查詢資料
         // 將  HashSet 轉 TreeSet
-        data = new TreeSet<>();
-        data.contains(100);
-        System.out.println(data);
+        data = new TreeSet<>(data);
+        System.out.println(data); // 承襲了 SortedSet 的特性(自然排序)
         System.out.println(data.getClass().getTypeName());
         
     }
