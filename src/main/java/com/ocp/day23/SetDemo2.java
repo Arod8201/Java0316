0ad7ff1528be7b3384170ca9f83fd7e2ebe61bad
Arod8201 <80758612+Arod8201@users.noreply.github.com>
2021-05-07 16:25:06 +0800

    Update SetDemo2.java

--- src/main/java/com/ocp/day23/SetDemo2.java
+++ src/main/java/com/ocp/day23/SetDemo2.java
@@ -8,39 +8,46 @@ import java.util.stream.Stream;
 public class SetDemo2 {
 
     public static void main(String[] args) {
-        // 國英數的分數是介面 0~100
-        Set scoers = new LinkedHashSet();
-        scoers.add(100);
-        //scoers.add(2000);
-        scoers.add(90);
-        scoers.add(80);
-        scoers.add(3.14);
-        scoers.add(true);
-        System.out.println(scoers);
+                // 國英數的分數是介於 0~100
+        Set scores = new LinkedHashSet();
+        scores.add(100);
+        scores.add(2000);
+        scores.add(90);
+        scores.add(80);
+        scores.add(3.14);
+        scores.add(true);
+        System.out.println(scores);
         System.out.println("=====================");
 
         // 過濾資料
-        Iterator iter = scoers.iterator();
+        Iterator iter = scores.iterator();
         while (iter.hasNext()) {
             Object next = iter.next();
-
-            if (next instanceof Double || next instanceof Boolean) {
+            if (next instanceof Integer) {
+                int score = (Integer) next;
+                if (!(score >= 0 && score <= 100)) {
+                    iter.remove();
+                }
+            } else {
                 iter.remove();
             }
-
         }
-        System.out.println(scoers);
+        System.out.println("介於 0~100 過濾後: " + scores);
+        System.out.println("=====================");
 
         // 求總分
+        //System.out.println(iter);
         int sum = 0;
-        iter = scoers.iterator(); // 要重新取得走訪器
+        iter = scores.iterator(); // 要重新取得走訪器
+//        System.out.println(scores);
+//        System.out.println(iter.getClass().getTypeName());
         while (iter.hasNext()) {
             Object next = iter.next();
-
             if (next instanceof Integer) {
+                //System.out.println(next.getClass().getTypeName());
                 sum += (Integer) next;
             }
         }
-        System.out.println(sum);
+        System.out.println("總分: " + sum);
     }
 }
