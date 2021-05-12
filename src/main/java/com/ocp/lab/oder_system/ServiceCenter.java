d795cc32aaacd45f138960276b645d763bf6bb53
Arod8201 <80758612+Arod8201@users.noreply.github.com>
2021-05-12 17:35:59 +0800

    Update ServiceCenter.java

--- src/main/java/com/ocp/lab/oder_system/ServiceCenter.java
+++ src/main/java/com/ocp/lab/oder_system/ServiceCenter.java
@@ -17,14 +17,15 @@ public class ServiceCenter {
     // 檢視 Catalog
     public void printCatalog() {
 
-        System.out.println("訂單系統");
-        System.out.println("+--------------------------+");
-        System.out.println("|No| 品茗 |尺寸|Price|");
-        System.out.println("+--------------------------+");
-        for (Food food : catalog) {
-            System.out.printf("|%4d|%s|　%c|%5d\n ", food.getNo(), food.getName(), food.getSize(), food.getPrice());
+      System.out.println("訂單列表");
+        System.out.println("+--------------------+");
+        System.out.println("|No|　品名　|尺寸|Price|");
+        System.out.println("+--------------------+");
+        for(Food food : catalog) {
+            System.out.printf("|%2d|%s|　%c|%5d|\n",
+                    food.getNo(), food.getName(), food.getSize(), food.getPrice());
         }
-        System.out.println("+--------------------------+");
+        System.out.println("+--------------------+");
     }
 
     // 檢視系統 menu
