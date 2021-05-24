89344dc7f2c39b1c6c6690adcc45fe7ef3a9c23f
sakur <sakur@DESKTOP-CMVLP18>
2021-05-25 03:09:28 +0800

    Java

--- src/main/java/com/ocp/day27/GroupingExam.java
+++ src/main/java/com/ocp/day27/GroupingExam.java
@@ -16,31 +16,40 @@ public class GroupingExam {
         );
 
         // 請問及格/不及格的人數分組
-        // true = 3 , false = 2}
+        // {true = 3, false = 2}
         System.out.println(
                 exams.stream()
                         .collect(Collectors.groupingBy(e -> e.getScore() >= 60))
         );
+        System.out.println("====================");
 
         System.out.println(
                 exams.stream()
-                        .collect(Collectors.groupingBy(e -> e.getScore() >= 60,
-                                Collectors.counting()))
+                        .collect(Collectors.groupingBy(e -> e.getScore() >= 60, Collectors.counting()))
         );
+        System.out.println("====================");
 
         // {true= [John, Hellen, Tom ], false = [Mary, Bob]}
         System.out.println(
                 exams.stream()
-                        .collect(Collectors.groupingBy(e -> e.getScore() >= 60 ? "及格" : "不及格",
+                        .collect(Collectors.groupingBy(e -> e.getScore() >= 60,
                                 Collectors.mapping(Exam::getName, toSet())))
         );
+        System.out.println("====================");
+
+        System.out.println(
+                exams.stream()
+                        .collect(Collectors.groupingBy(e -> e.getScore() >= 60 ? "及格" : "不及格",
+                                Collectors.mapping(Exam::getName, Collectors.toSet()))) // 直接 toSet() 也可以 , 不過要 import 
+        );
+        System.out.println("====================");
 
         // 如何把{不及格=[Bob, Mary], 及格=[Tom, John, Hellen]} 放入到 finalMap 中
         // 並印出 finalMap
         Map<String, Set<String>> finalMap = exams.stream()
                 .collect(Collectors.groupingBy(e -> e.getScore() >= 60 ? "及格" : "不及格",
                         Collectors.mapping(Exam::getName, toSet())));
-        System.out.println(finalMap);
+        System.out.println("finalMap: " + finalMap);
 
     }
 }
