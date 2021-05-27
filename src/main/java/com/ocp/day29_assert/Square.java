4c7dbcbf1ed1a0d1945884cdcf1caac9ea92bccb
sakur <sakur@DESKTOP-CMVLP18>
2021-05-27 22:31:03 +0800

    Java

---
+++ src/main/java/com/ocp/day29_assert/Square.java
@@ -0,0 +1,12 @@
+package com.ocp.day29_assert;
+
+// 用來繫計算面積的類別
+public class Square {
+
+    // 計算梯形面積
+    public int getArea(int top, int bottom, int h) {
+        int area = (top + bottom) * h / 2;
+        return area;
+    }
+
+}
