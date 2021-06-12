636f2debd37c8ba5d8de4116bd8ab06c38c03cb4
sakur <sakur@DESKTOP-CMVLP18>
2021-06-12 22:36:10 +0800

    Java

--- src/main/java/com/lab/cdc/Util.java
+++ src/main/java/com/lab/cdc/Util.java
@@ -28,20 +28,17 @@ public class Util {
         CDC[] cdcs = new Gson().fromJson(json, CDC[].class);
         return cdcs;
     }
-//       **
-//	 * 计算地球上任意两点(经纬度)距离
-//	 * 
-//	 * @param long1
-//	 *            第一点经度
-//	 * @param lat1
-//	 *            第一点纬度
-//	 * @param long2
-//	 *            第二点经度
-//	 * @param lat2
-//	 *            第二点纬度
-//	 * @return 返回距离 单位：米
-//	 */
 
+    /**
+     * 计算地球上任意两点(经纬度)距离
+     *
+     * @param long1 第一点经度
+     * @param lat1 第一点纬度
+     * @param long2 第二点经度
+     * @param lat2 第二点纬度
+     *
+     * @return 返回距离 单位：米
+     */
     public static double distance(String p1, String p2) {
         // p1 = "22.39 , 114.12"
         // p1.split(",")[0] 會得到 "22.39" 的字串
@@ -62,11 +59,8 @@ public class Util {
         double sa2, sb2;
         sa2 = Math.sin(a / 2.0);
         sb2 = Math.sin(b / 2.0);
-        d = 2
-                * R
-                * Math.asin(Math.sqrt(sa2 * sa2 + Math.cos(lat1)
-                        * Math.cos(lat2) * sb2 * sb2));
+        d = 2 * R * Math.asin(Math.sqrt(sa2 * sa2 + Math.cos(lat1)))
+                * Math.cos(lat2) * sb2 * sb2;
         return d;
-
     }
 }
