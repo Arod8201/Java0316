a4ace694abcc1b3823ca1736db35130348bcdcda
sakur <sakur@DESKTOP-CMVLP18>
2021-06-12 21:19:08 +0800

    Java 6/10

--- src/main/java/com/ocp/day36_io/ReadNews.java
+++ src/main/java/com/ocp/day36_io/ReadNews.java
@@ -1,14 +1,17 @@
 package com.ocp.day36_io;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class ReadNews {
 
     public static void main(String[] args) {
         String newspath = "src\\main\\java\\com\\ocp\\day36_io\\files\\news.txt";
-        try (FileReader fr = new FileReader(newspath);
-                BufferedReader br = new BufferedReader(fr);) {
+        try (FileInputStream fis = new FileInputStream(newspath);
+                // byte[] 轉 char [] 可以支援指定編碼
+                InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+                BufferedReader br = new BufferedReader(isr);) {
 
             String data = null;
             while ((data = br.readLine()) != null) {
@@ -18,7 +21,6 @@ public class ReadNews {
         }
     }
 }
-//(FileInputStream fis = new FileInputStream(newspath);
-//                // byte[] 轉 char [] 可以支援指定編碼
-//                InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
-//                BufferedReader br = new BufferedReader(isr);)
+
+//try (FileReader fr = new FileReader(newspath);
+//                BufferedReader br = new BufferedReader(fr);) 
